25e4f10eb8deab4e3eb4c8937082708adf7124a4
StigStack <jstig4@gmail.com>
2022-07-03 10:45:06 -0700

    added loveFunc.js solution

--- 8kyu/loveFunc.js
+++ 8kyu/loveFunc.js
@@ -5,8 +5,5 @@
 // My code
 
 function lovefunc(flower1, flower2){
-    const flower1Even = flower1 % 2 === 0 ? true : false
-    const flower2Even = flower2 % 2 === 0 ? true : false
-
-    return flower1Even !== flower2Even
+    return flower1 % 2 !== flower2 % 2
   }
